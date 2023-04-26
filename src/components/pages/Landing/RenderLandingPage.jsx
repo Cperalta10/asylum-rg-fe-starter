ae30e732ef6f8467a729e614b7f232182e1edfeb
Chris Peralta <chrisperalta1998@gmail.com>
2023-04-26 13:23:41 -0700

    finished adding the missing information on the landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,24 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div className="graph">
+          <img src={GrantRatesByOfficeImg} alt="" className="graph-img" />
+          <h3>Search Grant Rates By Office</h3>
+        </div>
+        <div className="graph">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt=""
+            className="graph-circle"
+          />
+          <h3>Search Grant Rates By Nationality</h3>
+        </div>
+        <div className="graph">
+          <img src={GrantRatesOverTimeImg} alt="" className="graph-img" />
+          <h3>Search Grant Rates Over Time</h3>
+        </div>
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -61,7 +78,43 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h2 className="bottom-title">Systemic Disparity Insights</h2>
+          <div className="info-section">
+            <div className="info-card">
+              <h3>36%</h3>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="info-card">
+              <h3>5%</h3>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+            <div className="info-card">
+              <h3>6x Lower</h3>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+          <button
+            style={{
+              border: '0',
+              backgroundColor: '#404C4A',
+              color: '#FFFFFF',
+            }}
+          >
+            Read More
+          </button>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
